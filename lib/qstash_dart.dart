3914935c44983bc5f86c83fc5b6331cd21cebace
rebaz94 <rebazrauf@gmail.com>
2022-10-13 01:50:45 +0300

    Import the model only

--- lib/qstash_dart.dart
+++ lib/qstash_dart.dart
@@ -2,7 +2,8 @@ library qstash_dart;
 
 export 'src/client/client.dart';
 export 'src/client/endpoints.dart';
-export 'src/client/http.dart' show UpstashRequest, UpstashResponse, Requester, RetryConfig;
+export 'src/client/http.dart'
+    show UpstashRequest, UpstashResponse, Requester, RetryConfig;
 export 'src/client/messages.dart';
 export 'src/client/platform/platform.dart';
 export 'src/client/schedules.dart';
